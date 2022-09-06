416c358669b2905591ae5291235fd34e71c30ddf
Deep Patel <dpatel@varicent.com>
2022-09-06 18:25:27 -0400

    Full rework for native code

--- android/src/main/java/com/microsoft/intune/mam/RNReactNativeMsIntuneMamPackage.java
+++ android/src/main/java/com/microsoft/intune/mam/RNReactNativeMsIntuneMamPackage.java
@@ -12,17 +12,14 @@ import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 
 public class RNReactNativeMsIntuneMamPackage implements ReactPackage {
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new RNReactNativeMsIntuneMamModule(reactContext));
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
     }
